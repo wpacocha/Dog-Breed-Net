f4647d2da00217cbcf935fc98bb398f1cc671ab6
Wojciech Pacocha <wpacocha1@icloud.com>
2025-04-05 19:33:42 +0200

    Add ResNet50 model with fine-tuning and training history plots. Tested successfully in Google Colab.

---
+++ src/plot_training.py
@@ -0,0 +1,23 @@
+import matplotlib.pylot as plt
+
+def plot_history(history,fine_tune_history=None):
+    acc = history.history['accuracy']
+    val_acc = history.history['val_accuracy']
+    loss = history.history['loss']
+    val_loss =history.history['val_loss']
+
+    if fine_tune_history:
+        acc += fine_tune_history.history['accuracy']
+        val_acc += fine_tune_history.history['val_accuracy']
+        loss += fine_tune_history.history['loss']
+        val_loss += fine_tune_history.history['val_loss']
+
+    epochs = range(1, len(acc)+1)
+
+    plt.figure(figsize = (12,5))
+    plt.subplot(1,2,1)
+    plt.plot(epochs,loss, 'bo-', label='Train loss')
+    plt.plot(epochs,val_loss,'r^-', label='Val loss')
+    plt.legend()
+    plt.title('Loss')
+    plt.show()
